53b961a2b1bfcaf011025f3d04ff5c6dd245a28d
Ryan Booker <7423+ryanbooker@users.noreply.github.com>
2025-12-24 09:22:24 +1000

    Initial Commit

---
+++ Flags/ContentView.swift
@@ -0,0 +1,19 @@
+// Copyright © 2025 Ryan Booker. All rights reserved.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flags/FlagsApp.swift
@@ -0,0 +1,12 @@
+// Copyright © 2025 Ryan Booker. All rights reserved.
+
+import SwiftUI
+
+@main
+struct FlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
